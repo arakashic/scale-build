0cbea6f80d2d33d4ccef6bbe5bebb4481258ad2f
Yanfei Guo <yanf.guo@gmail.com>
2024-11-27 14:21:36 -0600

    Add support for intel QAT to ZFS
    1. add intel-qat package
    2. update zfs configuration and dependency
    3. add convenient script
    4. increase tmpfs size to fix space issue during "update" step.
       Creating NVIDIA driver extension will fail with SIGBUS due to
       tmpfs being full.

---
+++ sudo_build.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+PARAM=${1:-all}
+export TRUENAS_VERSION=24.10.1-MASTER-qat
+export SKIP_SOURCE_REPO_VALIDATION=1
+
+case "$PARAM" in
+    release)
+        make checkout
+        PACKAGES=kernel PARALLEL_BUILDS=1 make packages
+        PACKAGES=kernel-dbg PARALLEL_BUILDS=1 make packages
+        make
+    ;;
+    all)
+        make
+    ;;
+    checkout)
+        make checkout
+    ;;
+    packages)
+        make packages
+    ;;
+    update)
+        make update
+    ;;
+    iso)
+        make iso
+    ;;
+    *)
+        PACKAGES=$PARAM PKG_DEBUG=1 PARALLEL_BUILDS=1 make packages
+    ;;
+esac
+
