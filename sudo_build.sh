72bdb891436f944d1bc031344bf3db11b9a14f7b
Yanfei Guo <yanf.guo@gmail.com>
2026-08-12 03:00:47 +0000

    Use distinct QAT 4.28 image version

--- sudo_build.sh
+++ sudo_build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 PARAM=${1:-all}
-VERSION=${2:-26.0.0-qzfs_ksmbd}
+VERSION=${2:-26.0.0-qzfs_ksmbd_qat428}
 export TRUENAS_VERSION=$VERSION
 export SKIP_SOURCE_REPO_VALIDATION=1
 export PARALLEL_BUILDS=${PARALLEL_BUILDS:-2}
